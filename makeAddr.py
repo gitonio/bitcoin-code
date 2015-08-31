c358dd6051f5095b4f90ebbcb7e0aed466a18b5c
gitonio <antonio.slaaf@gmail.com>
2015-08-31 15:34:35 -0700

    transactions 4

--- makeAddr.py
+++ makeAddr.py
@@ -2,7 +2,7 @@ import random, keyUtils, codecs, hashlib, utils, binascii
 #http://gobittest.appspot.com/
 #https://en.bitcoin.it/wiki/Wallet_import_format
 #http://kunststube.net/encoding/
-#private_key = ''.join(['%x' % random.randrange(16) for x in range(0, 64)])
+#private_key = ''.join(['%x' % random.randrange(16) for x in range(0, 64)]) 
 private_key = '0C28FCA386C7A227600B2FE50B7CAE11EC86D3BF1FBE471BE89827E19D72AA1D'
 print('****************************************')
 print('***** Private key to WIF ***************')
