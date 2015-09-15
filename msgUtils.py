f675257a330862876f666597e50693d6e213078f
gitonio <antonio.slaaf@gmail.com>
2015-09-15 12:42:14 -0700

    msgUtils passes

--- msgUtils.py
+++ msgUtils.py
@@ -4,7 +4,7 @@ import struct
 import time
 import socket
 import unittest
-
+import sys
 import utils
 
     
@@ -20,48 +20,47 @@ def processChunk(header, payload):
     """ Processes a response from a peer."""
     magic, cmd, payload_len, checksum = struct.unpack('L12sL4s', header)
     if len(payload) != payload_len:
-        print 'BAD PAYLOAD LENGTH', len(payload), payload_len
+        print ('BAD PAYLOAD LENGTH', len(payload), payload_len)
         
-    cmd = cmd.replace('\0', '') # Remove null termination
-    print '--- %s ---' % cmd
-    
-    if cmd == 'version':
+    cmd = cmd.decode().replace('\0', '').encode('utf-8') # Remove null termination
+    print ('--- %s ---' % cmd)
+    if cmd == b'version':
         version, services, timestamp, addr_recv, addr_from, nonce = struct.unpack('<LQQ26s26sQ', payload[:80])
         agent, agent_len = utils.processVarStr(payload[80:])
 
         start_height = struct.unpack('<L', payload[80 + agent_len:84 + agent_len])[0]
-        print '%d %x %x %s %s %x %s %x' % (
+        print ('%d %x %x %s %s %x %s %x' % (
             version, services, timestamp, utils.processAddr(addr_recv), utils.processAddr(addr_from),
-            nonce, agent, start_height)
-    elif cmd == 'inv':
+            nonce, agent, start_height))
+    elif cmd == b'inv':
         count, offset = utils.processVarInt(payload)
         result = []
         for i in range(0, count):
             type, hash = struct.unpack('<L32s', payload[offset:offset+36])
             # Note: hash is reversed
-            print type, hash[::-1].encode('hex')
+            print( type, hash[::-1])
             if type == 2:
                 sys.exit(0)
             result.append([type, hash])
             offset += 36
-        print '---\n'
+        print ('---\n')
         return result
-    elif cmd == 'addr':
+    elif cmd == b'addr':
         global addrCount
         count, offset = utils.processVarInt(payload)
         for i in range(0, count):
             timestamp, = struct.unpack('<L', payload[offset:offset+4])
             addr = utils.processAddr(payload[offset+4:offset+30])
             offset += 30
-            print addrCount, time.ctime(timestamp), addr
+            print (addrCount, time.ctime(timestamp), addr)
             addrCount += 1
     else:
         dump(payload)
-    print '---\n'
+    print ('---\n')
 
 
 def dump(s):
-    print ':'.join(x.encode('hex') for x in s)
+    print (':'.join(x for x in s))
 
 
 def getVersionMsg():
@@ -107,28 +106,27 @@ def getAddrMsg():
 
 class TestParsing(unittest.TestCase):        
     def test_processChunk(self):
-        header = ('\xf9\xbe\xb4\xd9\x76\x65\x72\x73\x69\x6f\x6e\x00\x00\x00\x00\x00' +
-                '\x66\x00\x00\x00\x85\xe6\xaa\x94')
-        payload = ('\x71\x11\x01\x00\x01\x00\x00\x00' +
-               '\x00\x00\x00\x00\xa2\x31\xa0\x52\x00\x00\x00\x00\x01\x00\x00\x00' +
-               '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff' +
-               '\x6c\x51\xe0\xee\xd8\x73\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-               '\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\x62\x91\x98\x16\x20\x8d' +
-               '\xf4\x7d\x37\xbf\xe4\xe7\x1f\xd2\x11\x2f\x53\x61\x74\x6f\x73\x68' +
-               '\x69\x3a\x30\x2e\x38\x2e\x32\x2e\x32\x2f\x02\x2b\x04\x00')
+        header = (b'\xf9\xbe\xb4\xd9\x76\x65\x72\x73\x69\x6f\x6e\x00\x00\x00\x00\x00' +
+                b'\x66\x00\x00\x00\x85\xe6\xaa\x94')
+        payload = (b'\x71\x11\x01\x00\x01\x00\x00\x00' +
+               b'\x00\x00\x00\x00\xa2\x31\xa0\x52\x00\x00\x00\x00\x01\x00\x00\x00' +
+               b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff' +
+               b'\x6c\x51\xe0\xee\xd8\x73\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
+               b'\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\x62\x91\x98\x16\x20\x8d' +
+               b'\xf4\x7d\x37\xbf\xe4\xe7\x1f\xd2\x11\x2f\x53\x61\x74\x6f\x73\x68' +
+               b'\x69\x3a\x30\x2e\x38\x2e\x32\x2e\x32\x2f\x02\x2b\x04\x00')
         processChunk(header, payload)
-
     def test_processInv(self):
-        header = ('\xf9\xbe\xb4\xd9\x69\x6e\x76\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
-                  '\x25\x00\x00\x00\x73\x3b\xf4\x95')
-        payload = ('\x01\x01\x00\x00\x00\xd4\xe5\xc2' +
-                   '\x8b\x09\x45\x05\xce\xec\x7c\x61\x34\xd1\xbd\x16\x80\x69\xc8\xc9' +
-                   '\xc3\x31\x93\xb5\x87\x27\xd9\xda\x7d\xa2\x80\x23\x20')
+        header = (b'\xf9\xbe\xb4\xd9\x69\x6e\x76\x00\x00\x00\x00\x00\x00\x00\x00\x00' +
+                  b'\x25\x00\x00\x00\x73\x3b\xf4\x95')
+        payload = (b'\x01\x01\x00\x00\x00\xd4\xe5\xc2' +
+                   b'\x8b\x09\x45\x05\xce\xec\x7c\x61\x34\xd1\xbd\x16\x80\x69\xc8\xc9' +
+                   b'\xc3\x31\x93\xb5\x87\x27\xd9\xda\x7d\xa2\x80\x23\x20')
         chunks = processChunk(header, payload)
         self.assertEqual(len(chunks), 1)
         self.assertEqual(chunks[0][0], 1) # MSG_TX
-        self.assertEqual(chunks[0][1], ('\xd4\xe5\xc2\x8b\x09\x45\x05\xce\xec\x7c\x61\x34\xd1\xbd\x16\x80' +
-'\x69\xc8\xc9\xc3\x31\x93\xb5\x87\x27\xd9\xda\x7d\xa2\x80\x23\x20'))
+        self.assertEqual(chunks[0][1], (b'\xd4\xe5\xc2\x8b\x09\x45\x05\xce\xec\x7c\x61\x34\xd1\xbd\x16\x80' +
+b'\x69\xc8\xc9\xc3\x31\x93\xb5\x87\x27\xd9\xda\x7d\xa2\x80\x23\x20'))
         
 
 if __name__ == '__main__':
